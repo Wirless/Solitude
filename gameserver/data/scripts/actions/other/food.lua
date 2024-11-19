53dab9b3116099db1d65a8e6b6a7701de3bf987b
IdleTibia <selppkez@gmail.com>
2024-11-19 17:00:32 +0100

    /

--- gameserver/data/scripts/actions/other/food.lua
+++ gameserver/data/scripts/actions/other/food.lua
@@ -1,43 +1,43 @@
 local foods = {
-	[2362] = 8, -- carrot
-	[2666] = 15, -- meat
-	[2667] = 12, -- fish
-	[2668] = 10, -- salmon
-	[2669] = 17, -- northern pike
-	[2670] = 4, -- shrimp
-	[2671] = 30, -- ham
-	[2672] = 60, -- dragon ham
-	[2673] = 5, -- pear
-	[2674] = 6, -- red apple
-	[2675] = 13, -- orange
-	[2676] = 8, -- banana
-	[2677] = 1, -- blueberry
-	[2678] = 18, -- coconut
-	[2679] = 1, -- cherry
-	[2680] = 2, -- strawberry
-	[2681] = 9, -- grapes
-	[2682] = 20, -- melon
-	[2683] = 17, -- pumpkin
-	[2684] = 8, -- carrot
-	[2685] = 6, -- tomato
-	[2686] = 9, -- corncob
-	[2687] = 2, -- cookie
-	[2688] = 2, -- candy cane
-	[2689] = 10, -- bread
-	[2690] = 3, -- roll
-	[2691] = 8, -- brown bread
-	[2695] = 6, -- egg
-	[2696] = 9, -- cheese
-	[2787] = 9, -- white mushroom
-	[2788] = 4, -- red mushroom
-	[2789] = 22, -- brown mushroom
-	[2790] = 30, -- orange mushroom
-	[2791] = 9, -- wood mushroom
-	[2792] = 6, -- dark mushroom
-	[2793] = 12, -- some mushrooms
-	[2794] = 3, -- some mushrooms
-	[2795] = 36, -- fire mushroom
-	[2796] = 5, -- green mushroom
+	[2362] = {5, "Crunch."}, -- carrot
+	[2666] = {15, "Munch."}, -- meat
+	[2667] = {12, "Munch."}, -- fish
+	[2668] = {10, "Mmmm."}, -- salmon
+	[2669] = {17, "Munch."}, -- northern pike
+	[2670] = {4, "Gulp."}, -- shrimp
+	[2671] = {30, "Chomp."}, -- ham
+	[2672] = {60, "Chomp."}, -- dragon ham
+	[2673] = {5, "Yum."}, -- pear
+	[2674] = {6, "Yum."}, -- red apple
+	[2675] = {13, "Yum."}, -- orange
+	[2676] = {8, "Yum."}, -- banana
+	[2677] = {1, "Yum."}, -- blueberry
+	[2678] = {18, "Slurp."}, -- coconut
+	[2679] = {1, "Yum."}, -- cherry
+	[2680] = {2, "Yum."}, -- strawberry
+	[2681] = {9, "Yum."}, -- grapes
+	[2682] = {20, "Yum."}, -- melon
+	[2683] = {17, "Munch."}, -- pumpkin
+	[2684] = {5, "Crunch."}, -- carrot
+	[2685] = {6, "Munch."}, -- tomato
+	[2686] = {9, "Crunch."}, -- corncob
+	[2687] = {2, "Crunch."}, -- cookie
+	[2688] = {2, "Munch."}, -- candy cane
+	[2689] = {10, "Crunch."}, -- bread
+	[2690] = {3, "Crunch."}, -- roll
+	[2691] = {8, "Crunch."}, -- brown bread
+	[2695] = {6, "Gulp."}, -- egg
+	[2696] = {9, "Smack."}, -- cheese
+	[2787] = {9, "Munch."}, -- white mushroom
+	[2788] = {4, "Munch."}, -- red mushroom
+	[2789] = {22, "Munch."}, -- brown mushroom
+	[2790] = {30, "Munch."}, -- orange mushroom
+	[2791] = {9, "Munch."}, -- wood mushroom
+	[2792] = {6, "Munch."}, -- dark mushroom
+	[2793] = {12, "Munch."}, -- some mushrooms
+	[2794] = {3, "Munch."}, -- some mushrooms
+	[2795] = {36, "Munch."}, -- fire mushroom
+	[2796] = {5, "Munch."}, -- green mushroom
 }
 
 local action = Action()
@@ -49,10 +49,11 @@ function action.onUse(player, item, fromPosition, target, toPosition)
 	end
 
 	local condition = player:getCondition(CONDITION_REGENERATION, CONDITIONID_DEFAULT)
-	if condition and math.floor(condition:getTicks() / 1000 + (food * 12)) >= 1200 then
+	if condition and math.floor(condition:getTicks() / 1000 + (food[1] * 12)) >= 1200 then
 		player:sendTextMessage(MESSAGE_STATUS_SMALL, "You are full.")
 	else
-		player:feed(food * 12)
+		player:feed(food[1] * 12)
+		player:say(food[2], TALKTYPE_SAY)
 		item:remove(1)
 	end
 	return true
